a328a306cc55efa4a1fe9ff609ffe9eecc78a4dc
kebin20 <ktanzyl@hotmail.com>
2022-11-23 21:54:17 +0900

    Fetched data from API using useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,6 @@ import QuizList from "./components/Quiz/QuizList";
 
 export default function App() {
   const [isShown, setIsShown] = React.useState(true);
-  const [quizData, setQuizData] = React.useState({});
-
-  fetch(
-    "https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple"
-  )
-    .then((res) => res.json())
-    .then((data) => console.log(data));
 
   function welcomeModalHandler() {
     setIsShown((isShown) => !isShown);
--- src/components/Quiz/QuizList.jsx
+++ src/components/Quiz/QuizList.jsx
@@ -7,6 +7,25 @@ import Card from "../UI/Card";
 import classes from "./QuizList.module.css";
 
 export default function QuizList(props) {
+  const [quiz, setQuiz] = React.useState({
+    question: "",
+    correctAnswer: "",
+    incorrectAnswer: "",
+  });
+
+  const [allQuiz, setAllQuiz] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(
+      "https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple"
+    )
+      .then((res) => res.json())
+      .then((data) => setAllQuiz(data.results));
+  }, []);
+
+  console.log(allQuiz);
+  console.log(quiz);
+
   return (
     <Card className={classes.quizlist}>
       <QuizItem />
