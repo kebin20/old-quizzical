71b6942e67b78885b83b6230910e54e2f7e505e0
kebin20 <ktanzyl@hotmail.com>
2022-11-26 10:24:46 +0900

    Managed to display the questions, not sure how to shuffle array of answers

--- src/components/Quiz/QuizItem.jsx
+++ src/components/Quiz/QuizItem.jsx
@@ -1,24 +1,43 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 
-import AnswerButton from "../UI/AnswerButton";
+// import AnswerButton from "../UI/AnswerButton";
 
 import classes from "./QuizItem.module.css";
 
 export default function QuizItem(props) {
   return (
-    <div className={classes.quizlist__quizitem}>
-      <h3 className={classes.quizitem__h3}>
-        {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-        tenetur? */}
-        {props.question}
-      </h3>
-      <div className={classes.quizitem__answers}>
-        <AnswerButton>ANSWER</AnswerButton>
-        <AnswerButton>ANSWER</AnswerButton>
-        <AnswerButton>ANSWER</AnswerButton>
-        <AnswerButton>ANSWER</AnswerButton>
-      </div>
+    <div>
+      {props.quizzes.map((quiz) => (
+        <div
+          key={Math.random().toString()}
+          className={classes.quizlist__quizitem}
+        >
+          <h3 className={classes.quizitem__h3}>
+            {quiz.question.replaceAll("&quot;", '"').replaceAll("&#039;", "'")}
+          </h3>
+          {/* <AnswerButton>{quiz.answerSelection}</AnswerButton>
+          <AnswerButton>{quiz.answerSelection}</AnswerButton>
+          <AnswerButton>{quiz.answerSelection}</AnswerButton>
+          <AnswerButton>{quiz.answerSelection}</AnswerButton> */}
+          {/* In this 'answerSelection', I want to randomise an array of answers which also contains the correct answer, 
+          therefore, this array will need to contain both the incorrect and correct answers, 
+          therefore, I will need to create a new array that contains both of these incorrect and correct answers,
+          thereby, I will need to somehow, take the value of the correct answer AND the value(in this case, the array of the incorrect answers) and JOIN them together to make a new AnswerSelection Array.
+          Once this array has been made, I will then need a function that will randomise the index number of this array and return said randomized index number.
+          Finally, use this randomised index number in the answer button. 
+          */}
+        </div>
+      ))}
     </div>
   );
 }
+
+// {
+//   /*
+//
+//
+//         <AnswerButton>ANSWER</AnswerButton>
+//         <AnswerButton>ANSWER</AnswerButton>
+//
+// }
--- src/components/Quiz/QuizList.jsx
+++ src/components/Quiz/QuizList.jsx
@@ -7,80 +7,21 @@ import Card from "../UI/Card";
 import classes from "./QuizList.module.css";
 
 export default function QuizList(props) {
-  const [quiz, setQuiz] = React.useState({
-    questions: "",
-    correctAnswer: "",
-    incorrectAnswer: "",
-  });
-
-  const [allQuiz, setAllQuiz] = React.useState([]);
+  const [quiz, setQuiz] = React.useState([]);
 
   React.useEffect(() => {
     fetch(
       "https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple"
     )
       .then((res) => res.json())
-      .then((data) => setAllQuiz(prevAllQuizData => data.results));
-  }, [quiz]);
+      .then((data) => setQuiz(data.results));
+  }, []);
 
-  function getQuizData() {
-    const randomNumber = Math.floor(Math.random() * allQuiz.length);
-    const question1 = allQuiz[0].question
-      .replaceAll("&quot;", '"')
-      .replaceAll("&#039;", "'");
-    const question2 = allQuiz[1].question
-      .replaceAll("&quot;", '"')
-      .replaceAll("&#039;", "'");
-    const question3 = allQuiz[2].question
-      .replaceAll("&quot;", '"')
-      .replaceAll("&#039;", "'");
-    const question4 = allQuiz[3].question
-      .replaceAll("&quot;", '"')
-      .replaceAll("&#039;", "'");
-    const question5 = allQuiz[4].question
-      .replaceAll("&quot;", '"')
-      .replaceAll("&#039;", "'");
-    setQuiz((prevQuiz) => ({
-      ...prevQuiz,
-      questions: {
-        first: question1,
-        second: question2,
-        third: question3,
-        fourth: question4,
-        fifth: question5,
-      },
-      id: Math.ceil(Math.random() * 6),
-    }));
-  }
+  console.log(quiz);
 
   return (
     <Card className={classes.quizlist}>
-      <QuizItem
-        question={quiz.questions.first}
-        correctAnswer={quiz.correctAnswer}
-        incorrectAnswers={quiz.incorrectAnswer}
-      />
-      <QuizItem
-        question={quiz.questions.second}
-        correctAnswer={quiz.correctAnswer}
-        incorrectAnswers={quiz.incorrectAnswer}
-      />
-      <QuizItem
-        question={quiz.questions.third}
-        correctAnswer={quiz.correctAnswer}
-        incorrectAnswers={quiz.incorrectAnswer}
-      />
-      <QuizItem
-        question={quiz.questions.fourth}
-        correctAnswer={quiz.correctAnswer}
-        incorrectAnswers={quiz.incorrectAnswer}
-      />
-
-      <QuizItem
-        question={quiz.questions.fifth}
-        correctAnswer={quiz.correctAnswer}
-        incorrectAnswers={quiz.incorrectAnswer}
-      />
+      <QuizItem quizzes={quiz} />
       <Button>Check Answers</Button>
     </Card>
   );
