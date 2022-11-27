a1cee5f1002bcc585b2190bfa55752ce11874de0
kebin20 <ktanzyl@hotmail.com>
2022-11-27 11:37:44 +0900

    FInally was able to show the randomised answer buttons

--- src/components/Quiz/QuizItem.jsx
+++ src/components/Quiz/QuizItem.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 
-// import AnswerButton from "../UI/AnswerButton";
+import AnswerButton from "../UI/AnswerButton";
 
 import classes from "./QuizItem.module.css";
 
@@ -9,35 +9,13 @@ export default function QuizItem(props) {
   return (
     <div>
       {props.quizzes.map((quiz) => (
-        <div
-          key={Math.random().toString()}
-          className={classes.quizlist__quizitem}
-        >
-          <h3 className={classes.quizitem__h3}>
-            {quiz.question.replaceAll("&quot;", '"').replaceAll("&#039;", "'")}
-          </h3>
-          {/* <AnswerButton>{quiz.answerSelection}</AnswerButton>
-          <AnswerButton>{quiz.answerSelection}</AnswerButton>
-          <AnswerButton>{quiz.answerSelection}</AnswerButton>
-          <AnswerButton>{quiz.answerSelection}</AnswerButton> */}
-          {/* In this 'answerSelection', I want to randomise an array of answers which also contains the correct answer, 
-          therefore, this array will need to contain both the incorrect and correct answers, 
-          therefore, I will need to create a new array that contains both of these incorrect and correct answers,
-          thereby, I will need to somehow, take the value of the correct answer AND the value(in this case, the array of the incorrect answers) and JOIN them together to make a new AnswerSelection Array.
-          Once this array has been made, I will then need a function that will randomise the index number of this array and return said randomized index number.
-          Finally, use this randomised index number in the answer button. 
-          */}
+        <div key={quiz.id} className={classes.quizlist__quizitem}>
+          <h3 className={classes.quizitem__h3}>{quiz.question}</h3>
+          {quiz.choices.map((choice, index) => {
+            return <AnswerButton key={index}>{choice}</AnswerButton>;
+          })}
         </div>
       ))}
     </div>
   );
 }
-
-// {
-//   /*
-//
-//
-//         <AnswerButton>ANSWER</AnswerButton>
-//         <AnswerButton>ANSWER</AnswerButton>
-//
-// }
--- src/components/Quiz/QuizList.jsx
+++ src/components/Quiz/QuizList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { nanoid } from "nanoid";
 
 import QuizItem from "./QuizItem";
 import Button from "../UI/Button";
@@ -14,7 +15,37 @@ export default function QuizList(props) {
       "https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple"
     )
       .then((res) => res.json())
-      .then((data) => setQuiz(data.results));
+      .then((data) => {
+        const dataArray = data.results;
+        const newDataArray = [];
+        dataArray.map((item) => {
+          return newDataArray.push({
+            question: item.question
+              .replaceAll("&quot;", '"')
+              .replaceAll("&#039;", "'"),
+            correct: item.correct_answer.replaceAll("&quot;", '"')
+            .replaceAll("&#039;", "'"),
+            // choices: [
+            //   { choice: item.correct_answer },
+            //   { choice: item.incorrect_answers[0] },
+            //   { choice: item.incorrect_answers[1] },
+            //   { choice: item.incorrect_answers[2] },
+            // ].sort(() => 0.5 - Math.random()),
+            choices: [
+              item.correct_answer.replaceAll("&quot;", '"')
+              .replaceAll("&#039;", "'"),
+              item.incorrect_answers[0].replaceAll("&quot;", '"')
+              .replaceAll("&#039;", "'"),
+              item.incorrect_answers[1].replaceAll("&quot;", '"')
+              .replaceAll("&#039;", "'"),
+              item.incorrect_answers[2].replaceAll("&quot;", '"')
+              .replaceAll("&#039;", "'")
+            ].sort(() => 0.5 - Math.random()),
+            id: nanoid(),
+          });
+        });
+        return setQuiz(newDataArray);
+      });
   }, []);
 
   console.log(quiz);
