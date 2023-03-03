0fd969334598448885141bacd92ec9a7ae19c29a
kebin20 <ktanzyl@hotmail.com>
2023-03-03 18:08:10 +0900

    build: installed styled components and changed some files to styled components

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,13 +1,24 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import classes from './Button.module.css'
+import styled from "styled-components";
+
+const StartButton = styled.button`
+  font-family: "Karla", sans-serif;
+  padding: 0.6em 2em;
+  border-radius: 12px;
+  border: none;
+  font-size: 1.3rem;
+  background-color: #4d5b9e;
+  color: white;
+  cursor: pointer;
+  align-self: center;
+  margin-top: 2em;
+`;
 
 export default function Button(props) {
-    return (
-        <button
-        className={classes.button}
-        type={props.type || "button"}
-        onClick={props.onClick}
-        >{props.children}</button>
-    )
+  return (
+    <StartButton type={props.type || "button"} onClick={props.onClick}>
+      {props.children}
+    </StartButton>
+  );
 }
--- src/components/UI/Welcome.jsx
+++ src/components/UI/Welcome.jsx
@@ -1,21 +1,38 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import styled from "styled-components";
 
 import classes from "./Welcome.module.css";
 
 import Card from "./Card";
 import Button from "./Button";
 
+const WelcomeTitle = styled.h1`
+  font-size: 4rem;
+  font-weight: 800;
+  margin-bottom: 0;
+`;
+
+const WelcomeSubtitle = styled.h3`
+  font-size: 1.2rem;
+`;
+
+const Footer = styled.footer`
+  padding: 1rem;
+  display: flex;
+  justify-content: flex-end;
+`;
+
 export default function Welcome(props) {
   return (
     <Card className={classes.modal}>
-      <h1 className={classes.welcome__h1}>Quizzical</h1>
-      <h3 className={classes.welcome__h3}>Fun trivia quiz for everyone!</h3>
-      <footer className="footer">
+      <WelcomeTitle>Quizzical</WelcomeTitle>
+      <WelcomeSubtitle>Fun trivia quiz for everyone!</WelcomeSubtitle>
+      <Footer>
         <Button type="button" onClick={props.onWelcomeModalHandler}>
           Start Quiz
         </Button>
-      </footer>
+      </Footer>
     </Card>
   );
 }
