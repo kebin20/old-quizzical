33d26a7dd51bf2e53a4a73a0c97455372bf73ff3
kebin20 <ktanzyl@hotmail.com>
2023-03-04 08:10:15 +0900

    refactor: changed styling of Card component from modules to styled components

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,10 +1,24 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import styled from "styled-components";
+import bgImage from "./assets/Background.svg";
 
-import classes from "./Card.module.css";
+const CardDiv = styled.div`
+  display: flex;
+  position: absolute;
+  top: 2%;
+  left: 50%;
+  padding: 2.5em;
+  transform: translate(-50%, 0);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url(${bgImage});
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 15px;
+`;
 
 export default function Card(props) {
-  return (
-    <div className={`${classes.card} ${props.className}`}>{props.children}</div>
-  );
+  return <CardDiv className={`${props.className}`}>{props.children}</CardDiv>;
 }
